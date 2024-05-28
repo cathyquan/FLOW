0c20155712899da1b3751a354c11822508d618d8
rafidchy03 <rafidchy03@gmail.com>
2024-05-28 12:12:48 +0000

    Added login functionality, connected frontend and backend, added user context, added database functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import LogInPage from "./pages/LogInPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import axios from "axios";
+import { UserContextProvider } from './UserContext.jsx';
+import { useEffect } from 'react';
+
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LogInPage />} />
-    </Routes>
+    <UserContextProvider> 
+      <Routes>
+        <Route path="/login" element={<LogInPage/>} />
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </UserContextProvider>
   )
 }
 
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
+
+function App() {
+    const {user} = useContext(UserContext);
+   return(
+    <div>
+        {!!user && (
+            <div> 
+                {user.email}
+            </div>
+        )}
+        Home page
+    </div>
+   );
+}
+
+export default App;
