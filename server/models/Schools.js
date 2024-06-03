e2308635713dfa9d79d54a39c4bfca0845fb7356
rafidchy03 <rafidchy03@gmail.com>
2024-06-03 15:11:06 +0000

    added logout functionality, added dynamic data info for shep/gcc, added edit user functionality for shep/gcc

--- server/models/Schools.js
+++ server/models/Schools.js
@@ -3,6 +3,9 @@ const {Schema} = mongoose;
 
 const schoolSchema = new Schema({
     schoolName: {type:String, unique:true, required:true},
+    SHEP: {type: Schema.Types.ObjectId, ref: 'User'},
+    GCC: {type: Schema.Types.ObjectId, ref: 'User'},
+    Classes: [{ type: Schema.Types.ObjectId, ref: 'Class' }],
 });
 
 const schoolModel = mongoose.model('School', schoolSchema);
