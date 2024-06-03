d672364d1130436630435737f90083fdaacb2c15
rafidchy03 <rafidchy03@gmail.com>
2024-06-03 23:56:24 +0000

    added school functionality w/ db & search filter for schools

--- client/src/pages/RenelHomePage.jsx
+++ client/src/pages/RenelHomePage.jsx
@@ -3,13 +3,14 @@ import axios from 'axios';
 import renel_logo from '../assets/images/renel_logo.png';
 import '../assets/style/RenelHomePage.css';
 import RenelNavbar from '../components/RenelNavbar.jsx';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function RenelHomePage() {
     const [schoolName, setSchoolName] = useState('');
     const [schools, setSchools] = useState([]);
     const [selectedSchool, setSelectedSchool] = useState('');
     const [action, setAction] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         // Fetch the list of schools for the dropdown
@@ -28,11 +29,19 @@ function RenelHomePage() {
             schoolName
         }).then(response => {
             console.log(response.data);
-            alert('school added!');
+            alert('School added!');
             setSchoolName('');
+            // Refresh the list of schools
+            axios.get('http://localhost:4000/getSchools')
+                .then(response => {
+                    setSchools(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         }).catch(error => {
-            console.error("there was an error!", error);
-            alert('school not added!');
+            console.error("There was an error!", error);
+            alert('School not added!');
         });
     };
 
@@ -52,7 +61,7 @@ function RenelHomePage() {
         }).then(response => {
             console.log(response.data);
             alert('School deleted!');
-            // Optionally, refresh the list of schools here
+            // Refresh the list of schools
             axios.get('http://localhost:4000/getSchools')
                 .then(response => {
                     setSchools(response.data);
@@ -66,6 +75,10 @@ function RenelHomePage() {
         });
     };
 
+    const filteredSchools = schools.filter(school =>
+        school.schoolName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
         <div className="renel-home-page">
             <header className="header">
@@ -77,37 +90,30 @@ function RenelHomePage() {
                     <RenelNavbar/>
                 </div>
             </header>
-            {/* this is the main content of the page */}
             <main className="main-content">
                 <section className="most-urgent">
                     <h1>Most Urgent</h1>
-
-                    <p><Link to="/home" className="school-link">School A</Link></p>
-                    <p><Link to="/home" className="school-link">School A</Link></p>
-                    <p><Link to="/home" className="school-link">School A</Link></p>
-                    <p><Link to="/home" className="school-link">School A</Link></p>
-                    <p><Link to="/home" className="school-link">School A</Link></p>
-                    <p><Link to="/home" className="school-link">School A</Link></p>
-                    <p><Link to="/home" className="school-link">School A</Link></p>
-
-
+                    {filteredSchools.slice(0, 7).map((school, index) => (
+                        <p key={index}><Link to="/home" className="school-link">{school.schoolName}</Link></p>
+                    ))}
                 </section>
                 <section className="all-schools">
                     <div className="search-container">
                         <link rel="stylesheet"
                               href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-                        <input type="text" placeholder="Search for a School" className="search-input"/>
+                        <input 
+                            type="text" 
+                            placeholder="Search for a School" 
+                            className="search-input"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
                         <button type="submit" className="search-button"><i className="fa fa-search"/></button>
                     </div>
                     <div className="school-list">
-                        {/*<h1>All Schools</h1>*/}
-                        <button><Link to="/home">School A</Link></button>
-                        <button><Link to="/home">School B</Link></button>
-                        <button><Link to="/home">School C</Link></button>
-                        <button><Link to="/home">School D</Link></button>
-                        <button><Link to="/home">School E</Link></button>
-                        <button><Link to="/home">School F</Link></button>
-                        <button><Link to="/home">School G</Link></button>
+                        {filteredSchools.map((school, index) => (
+                            <button key={index}><Link to="/home">{school.schoolName}</Link></button>
+                        ))}
                     </div>
                 </section>
                 <section className="manage-schools">
