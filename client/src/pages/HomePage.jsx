5b44eeeb74149ada44cfc9e234cbb71278f7308a
rafidchy03 <rafidchy03@gmail.com>
2024-06-24 22:58:16 +0000

    fixed admin bug

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -15,7 +15,7 @@ function App() {
     return (
         <div>
             {!!user ? (
-                user.userType === 'admin' ? (
+                user.userType === 'Administration' ? (
                     <RenelHomePage />
                 ) : (
                     <HomePage_new />
